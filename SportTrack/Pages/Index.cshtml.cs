4af9fabd30919cc318a0540ea9787774ad94614f
lukad <dujam.luka@gmail.com>
2025-07-02 23:09:43 +0200

    Updated home oage

--- SportTrack/Pages/Index.cshtml.cs
+++ SportTrack/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using SportTrack.Models;
 
 
 namespace SportTrack.Pages;
@@ -9,15 +11,20 @@ namespace SportTrack.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly SportTrack.Data.ApplicationDbContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(SportTrack.Data.ApplicationDbContext context)
     {
-        _logger = logger;
+        _context = context;
     }
 
-    public void OnGet()
-    {
+    public IList<SportEvent> SportEvent { get; set; } = default!;
 
+    public async Task OnGetAsync()
+    {
+        SportEvent = await _context.Events
+            .Include(s => s.AwayTeam)
+            .Include(s => s.HomeTeam)
+            .Include(s => s.Sport).ToListAsync();
     }
 }
